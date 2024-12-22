4dcdd5d0f09202b14fe14882b1e1df2aa8b06a53
Jihad Islam <jihadislam.diu@gmail.com>
2024-12-22 21:21:27 +0600

    a

--- 6. Python/100 days of Python/jihad.py
+++ 6. Python/100 days of Python/jihad.py
@@ -1,1 +1,22 @@
-print("Jihad Islam")
+'''
+    a. single quote string : vitore double quote use kora jay
+    b. double quote string : vitore single quote use kora jay
+    c. single triple single quote string : etay jebave likhbo shetai thakbe. line break use kora jay.
+    d. double triple double quote string : etatew jebave likhbo shetai thakbe. line break use kora jay.
+
+'''
+
+text = 'my name is "jihad" and I am a "developer"'
+print(text)
+
+text1 = '''
+    my name is "jihad" and I am a "developer".
+    Now I am learning python.
+    '''
+print(text1)
+
+text2 = """
+    my name is 'jihad' and I am a 'developer'.
+    Now I am learning python.
+    """
+print(text2)
